a36ee5866a5563583af3e016ba87bf39e97f355b
My Name <Me@gmail.com>
2022-10-14 00:35:54 +0400

    almost finished

--- ProgrammingTask3.py
+++ ProgrammingTask3.py
@@ -13,11 +13,17 @@ print(g)
 
 print()
 
-list1 = [1.2, 3.6, 5.0, 2.3, 10.19]
+list1 = [1.2, 3.6, 5.17, 2.3, 10.19]
+list2 = []
 c = 0
 for i in list1:
     d = list1[c] * 100 % 100
     e = round(d, 2)
+    list2.append(e)
     f = e / 100
     c += 1
-    print(f)
+    print(list2)
+sum = max(list2) + min(list2)
+result = sum / 100
+
+print(result)
