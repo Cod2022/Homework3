86af8ac0cea8d396ff2b86883e21ef7284db4fa1
My Name <Me@gmail.com>
2022-10-13 22:10:20 +0400

    task finished

--- ProgrammingTask2.py
+++ ProgrammingTask2.py
@@ -2,7 +2,16 @@
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
 list1 = [2, 10, 11, 25, 3]
-count = 0
-for i in list1:
-    sum = list1[count] + list1[-count]
-    print(sum)
+list2 = []
+count = 5
+
+for i in range(len(list1)):
+    while i < len(list1) / 2 and count > len(list1) / 2:
+        count = count -1
+        m = list1[i] * list1[count]
+        list2.append(m)
+        i = i + 1
+        
+
+print(f'Исходный список: {list1}')
+print(f'Произведения пар чисел списка: {list2}')
