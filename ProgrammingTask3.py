e34599af9e547d6174c0d081c1f7e50f06130351
My Name <Me@gmail.com>
2022-10-14 06:27:36 +0400

    finished

--- ProgrammingTask3.py
+++ ProgrammingTask3.py
@@ -1,16 +1,21 @@
+# Задайте список из вещественных чисел. 
+# Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+list1 = [1.2, 3.6, 5.17, 2.5, 10.19]
 
 
-list1 = [1.2, 3.6, 5.17, 2.3, 10.19]
-list2 = []
-c = 0
-for i in list1:
-    d = list1[c] * 100 % 100
-    e = round(d, 2)
-    f = e / 100
-    list2.append(f)
-    c += 1
-    print(list2)
-sum = max(list2) + min(list2)
-# result = sum / 100
+def max_min_sum (a):
+    list2 = []
+    c = 0
+    for i in a:
+        d = a[c] * 100 % 100
+        e = round(d, 2)
+        f = e / 100
+        list2.append(f)
+        c += 1
+    sum = max(list2) + min(list2)
+    return sum 
 
-print(sum)
+result = max_min_sum(list1)
+print(f'Исходный список: {list1}')
+print(f'Сумма максимального и минимального элементов дробной части: {result}')
